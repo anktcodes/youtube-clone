fe352e3b706db2092634b23308c2b4d863695978
Ankit <ap349500@gmail.com>
2026-09-07 04:58:25 +0000

    Added ensureDirectoryExistence, convertVid eo, setupDirectories, downloadRawVideo, uploadProcessedVideo, deleteFile, deleteRawVideo, deleteProcessed Video functions to the storage.ts

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -8,14 +8,16 @@ import ffmpeg from 'fluent-ffmpeg';
 
 const storage = new Storage();
 
-const rawVideoBucketName = "ac-youtube-clone-raw-videos";
-const processedVideoBucketName = "ac-youtube-clone-processed-videos";
+const rawVideoBucketName = "anktcodes-youtube-clone-raw-videos";
+const processedVideoBucketName = "anktcodes-youtube-clone-processed-videos";
 
 const localRawVideoPath = "./raw-videos";
 const localProcessedVideoPath = "./processed-videos";
 
 /* Creates the local directories for raw and processed videos. */
 export function setupDirectories(){
+    ensureDirectoryExistence(localRawVideoPath);
+    ensureDirectoryExistence(localProcessedVideoPath);
 }
 
 /**
@@ -35,4 +37,69 @@ export function convertVideo(rawVideoName: string, processedVideoName: string) {
             })
             .save(`${localProcessedVideoPath}/${processedVideoName}`);
     })
+}
+
+
+
+// download raw video function write jsdocs here
+export async function downloadRawVideo(fileName: string) {
+    await storage.bucket(rawVideoBucketName)
+    .file(fileName)
+    .download({destination: `${localRawVideoPath}/${fileName}`});
+
+    console.log(
+        `gs://${rawVideoBucketName}/{fileName} downloaded to ${localRawVideoPath}/${fileName}.`
+    )
+}
+
+
+export async function uploadProcessedVideo(fileName: string) {
+    const bucket = storage.bucket(processedVideoBucketName);
+
+    await bucket.upload(`${localProcessedVideoPath}/${fileName}`,{
+        destination: fileName
+    });
+
+    console.log(
+        `${localProcessedVideoPath}/${fileName} uploaded to gs://${processedVideoBucketName}/${fileName}.`
+    );
+
+    await bucket.file(fileName).makePublic();
+}
+
+
+function deleteFile(filePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if (fs.existsSync(filePath)) {
+            fs.unlink(filePath, (err) =>{
+                if (err) {
+                    console.log(`Failed to delete file at ${filePath}`, err);
+                    reject(err);
+                } else {
+                    console.log(`File deleted at ${filePath}`, err);
+                    resolve();
+                }
+            })
+        } else {
+            console.log(`File not found at ${filePath}, skipping the delete.`);
+            resolve();
+        }
+    })
+}
+
+
+export function deleteRawVideo(fileName: string){
+    return deleteFile(`${localRawVideoPath}/${fileName}`);
+}
+
+export function deleteProcessedVideo(fileName: string){
+    return deleteFile(`${localProcessedVideoPath}/${fileName}`);
+}
+
+//ensure a directory exists, if not then creating one
+function ensureDirectoryExistence(dirPath: string) {
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, {recursive: true}); // recursive: true enables creating nested directory
+        console.log(`Directory created at ${dirPath}`);
+    }
 }
